3ed663a09832d8feffcb611a1dfd700eb77472fe
Victor Mwachi <mudakizevictor@gmail.com>
2024-04-24 16:48:18 +0300

    update

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000
+app.use(express.json())
+
+const db =require("./models/index");
+const Product = db.product
+//console.log(db)
+
+
+app.get("/",async (req,res)=>{
+
+		const items = await Product.findAll()
+	
+	res.json(items)
+})
+app.get("/products",async (req,res)=>{
+
+	const items = await Product.findAll()
+	
+	res.json(items)
+})
+
+app.post("/add-product",async (req,res)=>{
+	try {
+		// statements
+		const item = await Product.create({
+			code:req.body.code,
+			name:req.body.name,
+			costPrice:req.body.costPrice,
+			unitOfMeasure:req.body.unitOfMeasure
+		});
+		item.save();
+
+		res.status(201).json("product added succesfully")
+	} catch(e) {
+		// statements
+		console.log(e);
+
+		res.json("Error,product was not added")
+	}
+
+	
+	
+})
+app.put("/update/:id", async (req,res)=>{
+
+	console.log(req.body.id);
+	console.log("above here");
+
+	/**
+	const item = await Product.update(
+		{code:"003"},
+		{
+			where:{id:3},
+		});
+	item.save()
+
+	res.json(item)**/
+})
+
+
+app.post("/delete",async (req,res)=>{
+
+const item = await Product.destroy({
+	where:{id:18}
+})
+
+res.json(item)
+})
+
+db.sequelize.sync({alter:true})
+app.listen(port,(req,res)=>{
+	console.log(`server running on port ......${port}`);
+})
---
+++ models/product.js
@@ -0,0 +1,58 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize,DataTypes)=>{
+
+	class Product extends Model {}
+
+	Product.init({
+		code:{
+			type:DataTypes.STRING,
+			allowNull:false,
+			unique:true
+		},
+
+		name:{
+			type:DataTypes.STRING,
+			allowNull:false,
+			unique:true
+		},
+		costPrice:{
+			type:DataTypes.INTEGER,
+			allowNull:false
+		},
+		unitOfMeasure:{
+			type:DataTypes.STRING,
+			allowNull:false
+		}
+		/**description:DataTypes.STRING,
+		category:DataTypes.STRING,
+		brand:DataTypes.STRING,
+		unitOfMeasure:DataTypes.STRING,
+		barcode:DataTypes.STRING,
+		costPrice:DataTypes.INTEGER,
+		sellingPrice:DataTypes.INTEGER,
+		packaging:DataTypes.STRING,
+		quantityAtHand:DataTypes.INTEGER,
+		reorderLevel:DataTypes.INTEGER,
+		images:DataTypes.STRING*/
+	},
+	{ sequelize, modelName:'product'});
+
+	return Product
+
+}
+
+/**
+ *    - Product ID/Code: A unique identifier for each product.
+   - Name: The name of the product.
+   - Description: A brief description of the product.
+   - Category: The category or type of the product (e.g., electronics, clothing, groceries).
+   - Brand: The brand associated with the product.
+   - Supplier Information: Information about the supplier or vendor.
+   - Cost Price: The price at which the product is purchased from the supplier.
+   - Selling Price: The price at which the product is sold to customers.
+   - Quantity on Hand: The current quantity of the product in stock.
+   - Reorder Level: The minimum quantity at which the product should be reordered.
+   - Images: Images of the product for visual reference.
+   */
+
